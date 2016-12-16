157f70ea8f5f9ee59d93045428611b4cb6e81e7f
YaJunCui <644583255@qq.com>
2016-12-16 13:45:35 +0800

    update deque.hpp

--- sgi_stl_learning/4_序列式容器/4_4_deque.hpp
+++ sgi_stl_learning/4_序列式容器/4_4_deque.hpp
@@ -3,14 +3,20 @@
 template <typename T, typename Alloc = alloc, size_t BuferSize = 0>
 class deque
 {
-public:                                                     //Basic types
+public:                                         //Basic types
   typedef T             value_type;
-  typedef value_type*   pointer;    
+  typedef value_type*   pointer;
 
 protected:
-  typedef pointer*      map_pointer;                              //元素指针的指针
+  typedef pointer*      map_pointer;                //元素指针的指针
 
-protected:                                                  //Data members
-  map_pointer map;                                                //map是连续空间，其内每个元素都是一个指针，指向一块缓冲区
-  size_type map_size;                                             //map内可容纳多少指针
+protected:                                      //Data members
+  map_pointer map;                                  //map是连续空间，其内每个元素都是一个指针，指向一块缓冲区
+  size_type map_size;                               //map内可容纳多少指针
 }
+
+template <typename T, typename Ref, typename Ptr, size_t BufSiz>
+struct __deque_iterator
+{
+
+};
