82cad765b185122014daa5aa40deae654e4d278d
YaJunCui <644583255@qq.com>
2016-12-16 20:17:52 +0800

    update 4_4_deque.hpp

--- sgi_stl_learning/4_序列式容器/4_4_deque.hpp
+++ sgi_stl_learning/4_序列式容器/4_4_deque.hpp
@@ -8,21 +8,6 @@ inline size_t __deque_buf_size(size_t n, size_t sz)
   return n !=0 ? n : (sz < 512 ? size_t(512 / sz) : size_t(1));
 }
 
-template <typename T, typename Alloc = alloc, size_t BuferSize = 0>
-class deque
-{
-public:                                         //Basic types
-  typedef T             value_type;
-  typedef value_type*   pointer;
-
-protected:
-  typedef pointer*      map_pointer;                //元素指针的指针
-
-protected:                                      //Data members
-  map_pointer map;                                  //map是连续空间，其内每个元素都是一个指针，指向一块缓冲区
-  size_type map_size;                               //map内可容纳多少指针
-}
-
 template <typename T, typename Ref, typename Ptr, size_t BufSiz>
 struct __deque_iterator
 {
@@ -156,5 +141,24 @@ struct __deque_iterator
     return (node == rhs.node) ? (cur < rhs.cur) : (node < rhs.node);
   }
 };
+
+template <typename T, typename Alloc = alloc, size_t BuferSize = 0>
+class deque
+{
+public:                                         //Basic types
+  typedef T             value_type;
+  typedef value_type*   pointer;
+  typedef size_t        size_type;
+
+public:
+  typedef __deque_iterator<T, T&, T*,BufSiz>  iterator;
+
+protected:
+  typedef pointer*      map_pointer;                //元素指针的指针
+
+protected:                                      //Data members
+  map_pointer map;                                  //map是连续空间，其内每个元素都是一个指针，指向一块缓冲区
+  size_type map_size;                               //map内可容纳多少指针
+}
 
 } //namespace cyj
