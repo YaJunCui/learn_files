ff6b0cb214a216dfdef3b231cd083d76f00f83bf
YaJunCui <644583255@qq.com>
2016-12-23 17:09:10 +0800

    add

---
+++ sgi_stl_learning/6_算法/6_3_1_numeric.cpp
@@ -0,0 +1,41 @@
+// Edit by cyj 2016-12-23
+
+#include <iostream>
+#include <numeric>
+#include <functional>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+  vector<int> iv{ 1, 2, 3, 4, 5 };
+
+  cout << accumulate(iv.begin(), iv.end(), 0) << endl;
+
+  cout << accumulate(iv.begin(), iv.end(), 0, minus<int>()) << endl;
+
+  cout << inner_product(iv.begin(), iv.end(), iv.begin(), 10) << endl;
+
+  cout << inner_product(iv.begin(), iv.end(), iv.begin(), 10, minus<int>(), plus<int>()) << endl;
+
+  ostream_iterator<int> oiter(cout, " ");
+
+  partial_sum(iv.begin(), iv.end(), oiter);
+  cout << endl;
+
+  partial_sum(iv.begin(), iv.end(), oiter, minus<int>());
+  cout << endl;
+
+  adjacent_difference(iv.begin(), iv.end(), oiter);
+  cout << endl;
+
+  adjacent_difference(iv.begin(), iv.end(), oiter, plus<int>());
+  cout << endl;
+
+  cout << pow(10, 3) << endl;
+
+
+  return 0;
+}
