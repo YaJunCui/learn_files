bd95d2ed195ea41c790ac273d551f037c9c87706
YaJunCui <644583255@qq.com>
2016-12-27 15:32:58 +0800

    add

--- sgi_stl_learning/6_算法/data_handle_funcs.hpp
+++ sgi_stl_learning/6_算法/data_handle_funcs.hpp
@@ -2,7 +2,13 @@
 #ifndef __CYJ_FUNCS_HPP__
 #define __CYJ_FUNCS_HPP__
 #include <iterator>
-#include <iostream>
+
+using std::input_iterator_tag;
+using std::forward_iterator_tag;
+using std::random_access_iterator_tag;
+using std::bidirectional_iterator_tag;
+using std::iterator_traits;
+using std::reverse_iterator;
 
 namespace cyj
 {
@@ -23,10 +29,10 @@ ForwardIterator adjacent_find(ForwardIterator first, ForwardIterator last)
 }
 
 template <class InputIterator, class T>
-typename std::iterator_traits<InputIterator>::difference_type
+typename iterator_traits<InputIterator>::difference_type
 count(InputIterator first, InputIterator last, const T& value)
 {
-  typename std::iterator_traits<InputIterator>::difference_type n = 0;
+  typename iterator_traits<InputIterator>::difference_type n = 0;
   for (; first != last; ++first)
   {
     if (*first == value)
@@ -36,7 +42,7 @@ count(InputIterator first, InputIterator last, const T& value)
 }
 
 template <class InputIterator, class Predicate>
-typename std::iterator_traits<InputIterator>::difference_type
+typename iterator_traits<InputIterator>::difference_type
 count_if(InputIterator first, InputIterator last, Predicate pred)
 {
   typename iterator_traits<InputIterator>::difference_type n = 0;
@@ -49,27 +55,27 @@ count_if(InputIterator first, InputIterator last, Predicate pred)
 }
 
 template<class RandomAccessIterator>
-typename std::iterator_traits<RandomAccessIterator>::difference_type
-__distance(RandomAccessIterator first, RandomAccessIterator last, std::random_access_iterator_tag)
+typename iterator_traits<RandomAccessIterator>::difference_type
+__distance(RandomAccessIterator first, RandomAccessIterator last, random_access_iterator_tag)
 {
-  return typename std::iterator_traits<RandomAccessIterator>::difference_type(last - first);
+  return typename iterator_traits<RandomAccessIterator>::difference_type(last - first);
 }
 
 template<class InputIterator>
-typename std::iterator_traits<InputIterator>::difference_type
-__distance(InputIterator first, InputIterator last, std::input_iterator_tag)
+typename iterator_traits<InputIterator>::difference_type
+__distance(InputIterator first, InputIterator last, input_iterator_tag)
 {
-  typename std::iterator_traits<InputIterator>::difference_type n = 0;
+  typename iterator_traits<InputIterator>::difference_type n = 0;
   for (; first != last; ++first)
     ++n;
   return n;
 }
 
 template <class InputIterator>
-typename std::iterator_traits<InputIterator>::difference_type
+typename iterator_traits<InputIterator>::difference_type
 distance(InputIterator first, InputIterator last)
 {
-  return __distance(first, last, typename std::iterator_traits<InputIterator>::iterator_category());
+  return __distance(first, last, typename iterator_traits<InputIterator>::iterator_category());
 }
 
 template <class ForwardIterator1, class ForwardIterator2>
@@ -78,8 +84,8 @@ inline ForwardIterator1 search(ForwardIterator1 first1,
                                ForwardIterator2 first2,
                                ForwardIterator1 last2)
 {
-  typedef typename std::iterator_traits<ForwardIterator1>::difference_type Distance1;
-  typedef typename std::iterator_traits<ForwardIterator2>::difference_type Distance2;
+  typedef typename iterator_traits<ForwardIterator1>::difference_type Distance1;
+  typedef typename iterator_traits<ForwardIterator2>::difference_type Distance2;
 
   Distance1 d1 = cyj::distance(first1, last1);
   Distance2 d2 = cyj::distance(first2, last2);
@@ -109,6 +115,74 @@ inline ForwardIterator1 search(ForwardIterator1 first1,
     }
   }
   return first1;
+}
+
+template <typename ForwardIterator1, typename ForwardIterator2>
+inline ForwardIterator1 find_end(ForwardIterator1 first1,
+                                 ForwardIterator1 last1,
+                                 ForwardIterator2 first2,
+                                 ForwardIterator2 last2,
+                                 forward_iterator_tag,
+                                 forward_iterator_tag)
+{
+  if (first2 == last2)                            //如果查询目标是空的
+    return first1;
+  else
+  {
+    ForwardIterator1 result = last1;
+    while (1)
+    {
+      //利用search()查找某个子序列，找不到话返回last1
+      ForwardIterator1 new_result = cyj::search(first1, last1, first2, last2);
+      if (new_result == last1)
+        return result;
+      else
+      {
+        result = new_result;
+        first1 = new_result;
+        ++first1;
+      }
+    }
+  }
+}
+
+template <typename BidirectionalIterator1, typename BidirectionalIterator2>
+inline BidirectionalIterator1 __find_end(BidirectionalIterator1 first1,
+                                         BidirectionalIterator1 last1,
+                                         BidirectionalIterator2 first2,
+                                         BidirectionalIterator2 last2,
+                                         bidirectional_iterator_tag,
+                                         bidirectional_iterator_tag)
+{
+  //利用反向查找，reverse_iterator
+  typedef reverse_iterator<BidirectionalIterator1> reviter1;
+  typedef reverse_iterator<BidirectionalIterator2> reviter2;
+
+  reviter1 rlast1(first1);
+  reviter2 rlast2(first2);
+  //查找时将序列1和序列2逆序
+  reviter1 rresult = cyj::search(reviter1(last1), rlast1,
+                                 reviter2(last2), rlast2);
+  if (rresult == rlast1)                    //没有找到
+    return last1;
+  else
+  {
+    BidirectionalIterator1 result = rresult.base();
+    std::advance(result, -cyj::distance(first2, last2));
+    return result;
+  }
+}
+
+template <typename ForwardIterator1, typename ForwardIterator2>
+inline ForwardIterator1 find_end(ForwardIterator1 first1,
+                                 ForwardIterator1 last1,
+                                 ForwardIterator2 first2,
+                                 ForwardIterator2 last2)
+{
+  typedef typename iterator_traits<ForwardIterator1>::iterator_category category1;
+  typedef typename iterator_traits<ForwardIterator2>::iterator_category category2;
+
+  return __find_end(first1, last1, first2, last2, category1(), category2());
 }
 
 }//namespace cyj
--- sgi_stl_learning/6_算法/data_handle_funcs_test.cpp
+++ sgi_stl_learning/6_算法/data_handle_funcs_test.cpp
@@ -7,14 +7,17 @@
 #include <list>
 #include <functional>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::list;
 
 int main()
 {
-  //vector<int> iv{ 0, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8 };
-  //vector<int> iv1{ 0, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8 };
-  vector<int> iv{ 0, 0, 1, 0, 0, 0, 1 };
-  vector<int> iv1{ 0, 0, 0, 1 };
+  vector<int> iv{ 0, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8 };
+  vector<int> iv1{ 0, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8 };
+  //vector<int> iv{ 0, 0, 1, 0, 0, 0, 1 };
+  //vector<int> iv1{ 0, 0, 0, 1 };
   list<int> ilist(iv.begin(), iv.end());
 
   cout << "test cyj::adjacent_find" << endl;
@@ -24,7 +27,7 @@ int main()
   cout << cyj::count(iv.begin(), iv.end(), 6) << endl;
 
   cout << "test cyj::count_if" << endl;
-  cout << cyj::count_if(iv.begin(), iv.end(), bind(equal_to<int>(),placeholders::_1,7)) << endl;
+  cout << cyj::count_if(iv.begin(), iv.end(), std::bind(std::equal_to<int>(),std::placeholders::_1,7)) << endl;
 
   //cout << "test diatance" << endl;
   //cout << cyj::distance(iv.begin(), iv.end()) << endl;
@@ -36,6 +39,10 @@ int main()
   auto iter = cyj::search(iv.begin(), iv.end(), iv1.begin(), iv1.end());
   if (iter != iv.end())
     cout << cyj::distance(iv.begin(), iter) << endl;
+
+  cout << "test find_end" << endl;
+  vector<int> iv2(iv.begin() + 6, iv.begin() + 8);
+  cout << *(cyj::find_end(iv.begin(), iv.end(), iv2.begin(), iv2.end()) + 3) << endl;
 
   return 0;
 }
