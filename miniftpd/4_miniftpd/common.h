e90334b89a240203d61c75975f62b9ec4089df5b
YaJunCui <644583255@qq.com>
2016-12-30 15:40:00 +0800

    add

---
+++ miniftpd/4_miniftpd/common.h
@@ -0,0 +1,26 @@
+//Edit by cyj 2016-12-30
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define  ERR_EXIT(m) \
+  do \
+  { \
+    perror(m); \
+    exit(EXIT_FAILURE); \
+  } \
+  while(0)
+
+#endif //__COMMON_H__
---
+++ miniftpd/4_miniftpd/main.c
@@ -0,0 +1,13 @@
+//Edit by cyj 2016-12-30
+#include "common.h"
+#include "sysutil.h"
+
+int main()
+{
+  if(getuid()!=0)
+  {
+    fprintf(stderr,"miniftpd : must ")
+  }
+
+  return 0;
+}
