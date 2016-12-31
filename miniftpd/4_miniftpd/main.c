03d768151fdf3e124863a9ab1522c05ea731dddc
YaJunCui <644583255@qq.com>
2016-12-31 16:08:03 +0800

    add

--- miniftpd/4_miniftpd/main.c
+++ miniftpd/4_miniftpd/main.c
@@ -1,14 +1,17 @@
 //Edit by cyj 2016-12-30
 
-#include "common.h"
+//#include "common.h"
+#include <stdio.h>
 
 int main()
 {
-  if(getuid()!=0)
-  {
-    fprintf(stderr,"miniftpd : must be started as root\n");
-    exit(EXIT_FAILURE);
-  }
+  // if(getuid()!=0)
+  // {
+  //   fprintf(stderr,"miniftpd : must be started as root\n");
+  //   exit(EXIT_FAILURE);
+  // }
+
+  printf("test\n");
 
   return 0;
 }
