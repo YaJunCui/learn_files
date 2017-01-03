1d2160ca0cc3048df9fc4a6f20d8a52d126479c0
YaJunCui <644583255@qq.com>
2017-01-03 09:50:13 +0800

    modify main.c

--- miniftpd/main.c
+++ miniftpd/main.c
@@ -9,7 +9,7 @@ int main()
   char *str1 = "    ab";
   char *str2 = "     ";
 
-  if(str_all_space(str1))
+  if (str_all_space(str1))
   {
     printf("str1 all space\n");
   }
@@ -18,7 +18,7 @@ int main()
     printf("str1 not all space\n");
   }
 
-    if(str_all_space(str2))
+  if (str_all_space(str2))
   {
     printf("str2 all space\n");
   }
@@ -31,36 +31,35 @@ int main()
   str_upper(str3);
   printf("%s\n", str3);
 
-
-  if(getuid()!=0)
+  if (getuid() != 0)
   {
-    fprintf(stderr,"miniftpd : must be started as root\n");
+    fprintf(stderr, "miniftpd : must be started as root\n");
     exit(EXIT_FAILURE);
   }
 
-  session_t sess = 
-  {
-    -1,"","","",                    //控制连接
-    -1,-1                        //父子进程通道
-  };
+  session_t sess =
+      {
+          -1, "", "", "", //控制连接
+          -1, -1          //父子进程通道
+      };
 
-  int listen_fd = tcp_server(NULL,5188);
+  int listen_fd = tcp_server(NULL, 5188);
   int conn;
   pid_t pid;
 
   while (1)
   {
     conn = accept_timeout(listen_fd, NULL, 0);
-    if(conn == -1)
+    if (conn == -1)
     {
       ERR_EXIT("accept_timeout");
     }
 
     pid = fork();
-    if(pid == -1)
+    if (pid == -1)
       ERR_EXIT("fork");
 
-    if(pid == 0)
+    if (pid == 0)
     {
       close(listen_fd);
       sess.ctrl_fd = conn;
