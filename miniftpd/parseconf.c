4bd95888f44dec9327d421eae2aedd33ab4a2d18
YaJunCui <644583255@qq.com>
2017-01-03 11:04:59 +0800

    add

---
+++ miniftpd/parseconf.c
@@ -0,0 +1,13 @@
+// Edit by cyj 2017-1-3
+
+#include "parseconf.h"
+
+void parseconf_load_file(const char *path)
+{
+
+}
+
+void parseconf_load_setting(const char *setting)
+{
+  
+}
---
+++ miniftpd/parseconf.h
@@ -0,0 +1,9 @@
+// Edit by cyj 2017-1-3
+
+#ifndef __PARSE_CONF_H__
+#define __PARSE_CONF_H__
+
+void parseconf_load_file(const char *path);
+void parseconf_load_setting(const char *setting);
+
+#endif  // _PARSE_CONF_H__
