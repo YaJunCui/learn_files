214ef15426933458e4cbc0d65aa2b1f05cd11646
YaJunCui <644583255@qq.com>
2017-01-06 21:49:19 +0800

    add

--- UNP/miniftpd/privsock.h
+++ UNP/miniftpd/privsock.h
@@ -4,30 +4,30 @@
 #include "privsock.h"
 #include "session.h"
 
-// FTP服务进程向nobody进程请求的命令
+// FTP锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷nobody锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
 #define PRIV_SOCK_GET_DATA_SOCK     1
 #define PRIV_SOCK_PASV_ACTIVE       2
 #define PRIV_SOCK_PASV_LISTEN       3
 #define PRIV_SOCK_PASV_ACCEPT       4
 
-// nobody进程对FTP服务进程的应答
+// nobody锟斤拷锟教讹拷FTP锟斤拷锟斤拷锟斤拷锟教碉拷应锟斤拷
 #define PRIV_SOCK_RESULT_OK         1
 #define PRIV_SOCK_RESULT_BAD        2
 
-// 用于内部进程通信初始化和设置的函数
+// 锟斤拷锟斤拷锟节诧拷锟斤拷锟斤拷通锟脚筹拷始锟斤拷锟斤拷锟斤拷锟矫的猴拷锟斤拷
 void priv_sock_init(session_t *sess);
 void priv_sock_close(session_t *sess);
 void priv_sock_set_parent_context(session_t *sess);
 void priv_sock_set_child_context(session_t *sess);
 
 
-// 用于内部进程互相发送命令和接收命令应答的函数
+// 锟斤拷锟斤拷锟节诧拷锟斤拷锟教伙拷锟洁发锟斤拷锟斤拷锟斤拷锟酵斤拷锟斤拷锟斤拷锟斤拷应锟斤拷锟侥猴拷锟斤拷
 void priv_sock_send_cmd(int fd, char cmd);
 char priv_sock_get_cmd(int fd);
 void priv_sock_send_result(int fd, char res);
 char priv_sock_get_result(int fd);
 
-// 用于内部进程互相发送端口/ip字符串/文件描述符和接收应答的函数
+// 锟斤拷锟斤拷锟节诧拷锟斤拷锟教伙拷锟洁发锟酵端匡拷/ip锟街凤拷锟斤拷/锟侥硷拷锟斤拷锟斤拷锟斤拷锟酵斤拷锟斤拷应锟斤拷锟侥猴拷锟斤拷
 void priv_sock_send_int(int fd, int the_int);
 int priv_sock_get_int(int fd);
 void priv_sock_send_buf(int fd, const char *buf, unsigned int len);
