97e5f1ce1f80020e7eedf785c0f80ac5b4effb2c
YaJunCui <644583255@qq.com>
2017-01-14 17:34:15 +0800

    modify

--- hash/main.c
+++ hash/main.c
@@ -1,9 +1,24 @@
 #include "hash.h"
 #include "common.h"
 
+typedef struct stu
+{
+  char sno[5];
+  char name[32];
+  int age;
+}stu_t;
+
 int main()
 {
   printf("main.\n");
+
+  stu_t stu_arr[] = 
+  {
+    {"1234","AAAA", 20},
+    {"4568", "BBBB", 23},
+    {"6729", "AAAA", 19}
+  };
+
 
   return 0;
 }
