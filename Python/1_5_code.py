4b99b38fba4094acfa600da6b2d64bee16cdbebd
YaJunCui <644583255@qq.com>
2017-03-22 15:44:56 +0800

    add

--- Python/1_5_code.py
+++ Python/1_5_code.py
@@ -1,10 +1,22 @@
 # coding:utf-8
 
-name = raw_input("Input your name : ")
-age = raw_input("How old are you?")
+# name = raw_input("Input your name : ")
+# age = raw_input("How old are you?")
 
-print "Your name is : ", name
-print "You are " + age + " years old."
+# print "Your name is : ", name
+# print "You are " + age + " years old."
 
-after_ten = int(age) + 10
-print "You will be " + str(after_ten) + " years old after ten years"
+# after_ten = int(age) + 10
+# print "You will be " + str(after_ten) + " years old after ten years"
+
+
+a = "xing:cui;ming:jun;"
+tmp_a = a[:-1]
+lst = tmp_a.split(";")
+i = 1
+
+for val in lst:
+  s_val = val.split(":")
+  for s in s_val:
+    print s,
+  print
