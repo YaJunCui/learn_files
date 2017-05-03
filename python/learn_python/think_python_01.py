9768b3f8011f2b9dc3fe53e40ca17276d26fd155
YaJunCui <644583255@qq.com>
2017-05-03 14:47:09 +0800

    add

---
+++ python/learn_python/think_python_01.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# coding=utf-8
+import math
+
+# x = 0
+# if x > 0:
+#     y = x**2
+# else:
+#     y = float('nan')
+
+# print y
+
+num = 100
+def func():
+    num += 100
+    print(num)
+
+func()
